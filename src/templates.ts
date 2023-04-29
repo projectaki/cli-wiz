7ce71473275b2f88c2bfbcc07049f349f868e6c4
Akos <akosegypro@gmail.com>
2023-04-29 17:40:28 +0900

    init

---
+++ src/templates.ts
@@ -0,0 +1,10 @@
+export const templates = {
+    default: {
+        name: 'Default',
+        value: 'default',
+    },
+    codeGen: {
+        name: 'Code generation',
+        value: 'codeGen',
+    },
+}
