25b5d8008d2ca1e69c13818a81622633e303c71b
Akos <akosegypro@gmail.com>
2023-04-30 16:15:52 +0900

    refac to use clack

--- src/ai/llm.ts
+++ src/ai/llm.ts
@@ -2,10 +2,19 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { PromptDict, prompts, TypedLLMChain } from "./prompt.js";
 import { LLMChainInput } from "langchain/chains";
 import { BufferMemory } from "langchain/memory";
+import color from "picocolors";
 
 export const OPENAI_CHAT_CLIENT = new ChatOpenAI({
   temperature: 0.9,
   modelName: "gpt-3.5-turbo",
+  streaming: true,
+  callbacks: [
+    {
+      handleLLMNewToken(token: string) {
+        process.stdout.write(color.bold(color.white(token)));
+      },
+    },
+  ],
 });
 
 export function createLLMChain<K extends keyof PromptDict>(promptType: K, options: Partial<LLMChainInput> = {}) {
--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,48 +1,37 @@
-import inquirer from "inquirer";
-import chalk from "chalk";
+import * as p from "@clack/prompts";
+import { setTimeout } from "node:timers/promises";
+import color from "picocolors";
+
 import { typedKeys } from "../utils.js";
 import { prompts } from "../ai/prompt.js";
-import { OPENAI_CHAT_CLIENT, createLLMChain } from "../ai/llm.js";
-import { LLMChain } from "langchain/chains";
+import { createLLMChain } from "../ai/llm.js";
+
+import util from "util";
+import { exec } from "child_process";
+
+const execPromise = util.promisify(exec);
 
 const options = typedKeys(prompts).map((key) => ({
-  name: prompts[key].name,
+  label: prompts[key].name,
   value: key,
 }));
 
 export async function run() {
-  const res = await inquirer.prompt([
-    {
-      type: "list",
-      name: "selectedOption",
-      message: chalk.blue("What type of ai assistance do you want?"),
-      choices: options,
-      prefix: chalk.blue("🤖"),
-    },
-  ]);
+  try {
+    const select = await p.select({ message: "Select a prompt", options });
 
-  const chain = createLLMChain(res.selectedOption, {
-    verbose: true,
-  });
+    const chain = createLLMChain(select as any, {
+      verbose: false,
+    });
 
-  while (true) {
-    try {
-      const answer = await inquirer.prompt([
-        {
-          type: "editor",
-          name: "prompt",
-          message: "What is your prompt?",
-        },
-      ]);
+    const answer = await p.text({
+      message: color.green("Whats your question?"),
+    });
 
-      const res = await chain.call({
-        prompt: answer.prompt,
-      });
-
-      console.log(res.text);
-    } catch (error) {
-      console.error("An error occurred:", error);
-      break;
-    }
+    const res = await chain.call({
+      prompt: answer as string,
+    });
+  } catch (error) {
+    console.error("An error occurred:", error);
   }
 }
